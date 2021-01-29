c15230b1d58c6902344ac366967094db00494f5b
Faisal Akhlaq <faisal.akhlaq@aol.com>
2021-01-29 15:03:12 +0100

    partners signup

--- customers/forms.py
+++ customers/forms.py
@@ -1,17 +1,15 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from core.models import Item, ItemImage
-# from .models import SupplierEmployee
+from .models import SupplierEmployee, Supplier
 
-# class UserForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ('username', 'first_name', 'last_name', 'email', 'password')
 
-# class SupplierEmployeeForm(forms.ModelForm):
-#     class Meta:
-#         model = SupplierEmployee
-#         fields = __all__
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
 
 class ItemForm(forms.ModelForm):
     
@@ -19,10 +17,21 @@ class ItemForm(forms.ModelForm):
         model = Item
         fields = '__all__'
 
-
 class ItemImageForm(forms.ModelForm):
     # TODO provide a delete button
     image = forms.ImageField(label='Item Image')    
     class Meta:
         model = ItemImage
         fields = ('image', )
+
+class CreateSupplierForm(forms.ModelForm):
+    
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+class CreateSupplierEmployeeForm(forms.ModelForm):
+    
+    class Meta:
+        model = SupplierEmployee
+        fields = '__all__'
--- customers/urls.py
+++ customers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PartnersHome
+from .views import PartnersHome, SupplierEmployee
 
 app_name = 'customers'
 
 urlpatterns = [
     path('partners_home', PartnersHome.as_view(), name='partners_home'),
+    path('employee_signup', SupplierEmployee.as_view(), name='employee_signup'),
 ]
